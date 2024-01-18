d2f5a3e7bd4e25c567d64b8616dba302856248a2
aborgardt <a.borgardt@sdzecom.de>
2024-01-18 16:38:46 +0100

    Save the state

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -1,7 +1,7 @@
 return {
   "kdheepak/lazygit.nvim",
-  -- optional for floating window border decoration
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  keys = { { "<leader>gg", ":LazyGit <CR>", desc = "Open LazyGit window." } },
 }
