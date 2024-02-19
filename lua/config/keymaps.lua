983ecaa1992ea7e3aeaaba02a11a74dc46c39477
aborgardt <a.borgardt@sdzecom.de>
2024-02-19 11:40:13 +0100

    execute command

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
-vim.keymap.set("n", "<leader>bw", ":bw", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bw", ":bw<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>q", ":q", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>cf", ":let @+=expand('%')<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>cF", ":let @+=expand('%:p')<CR>", { noremap = true, silent = true })
